6affbb6489478f86d9cbd8dc0f804d7aa666c5a7
Farid <novuzovfar1d@mail.ru>
2023-04-23 22:42:49 +0300

    Added error handler

--- src/main/java/com/reckue/ms/handler/exception/metric/MetricNotFoundException.java
+++ src/main/java/com/reckue/ms/handler/exception/metric/MetricNotFoundException.java
@@ -1,2 +1,17 @@
-package com.reckue.ms.handler.exception.metric;public class MetricNotFoundException {
+package com.reckue.ms.handler.exception.metric;
+
+import com.reckue.ms.handler.exception.NotFoundException;
+
+import java.util.Map;
+import java.util.UUID;
+
+public class MetricNotFoundException extends NotFoundException {
+
+    public MetricNotFoundException(UUID id) {
+        super(
+                "METRIC-0001",
+                String.format("Metric \"%s\" not found", id),
+                Map.of("id", id)
+        );
+    }
 }
